97764eaaf59ffa01a5f1f95a6e016f8e319dfe49
Zach Dahl <z.schtauffen@gmail.com>
2019-08-31 10:32:43 -0500

    add graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,19 @@
-import * as React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react'
+import ApolloClient, { gql } from 'apollo-boost'
+import { ApolloProvider, Query } from 'react-apollo'
+import './App.css'
 
-import { useLoading } from '@swyx/hooks';
-
-function LambdaDemo() {
-  const [isLoading, load] = useLoading();
-  const [msg, setMsg] = React.useState(null);
-  const handleClick = (api: string) => (
-    e: React.MouseEvent<HTMLButtonElement>
-  ) => {
-    e.preventDefault();
-    load(
-      fetch('/.netlify/functions/' + api)
-        .then(response => response.json())
-        .then(json => setMsg(json.msg))
-    );
-  };
-
-  return (
-    <p>
-      <button onClick={handleClick('hello')}>
-        {isLoading ? 'Loading...' : 'Call Lambda'}
-      </button>
-      <button onClick={handleClick('async-chuck-norris')}>
-        {isLoading ? 'Loading...' : 'Call Async Lambda'}
-      </button>
-      <br />
-      <span>{msg}</span>
-    </p>
-  );
-}
+const client = new ApolloClient({
+  uri: '/.netlify/functions/graphql'
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <LambdaDemo />
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Query query={gql`{ hello }`}>
+        {({ data }: any) => <div>Server says: {data.hello}</div>}
+      </Query>
+    </ApolloProvider>
   );
 }
 
