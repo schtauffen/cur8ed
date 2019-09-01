f5b021dcc4cc47871163130f5a84a81dce3e1402
Zach Dahl <z.schtauffen@gmail.com>
2019-09-01 09:40:56 -0500

    add dog photo

--- src/lambda/graphql.ts
+++ src/lambda/graphql.ts
@@ -3,12 +3,14 @@ const { ApolloServer, gql } = require('apollo-server-lambda')
 const typeDefs = gql`
   type Query {
     hello: String
+    dogPhotoUrl: String
   }
 `
 
 const resolvers = {
   Query: {
-    hello: (root, args, context) => 'Hello, world!'
+    hello: (root, args, context) => 'Hello, world!',
+    dogPhotoUrl: (root, args, context) => 'https://images.dog.ceo/breeds/pomeranian/n02112018_1090.jpg',
   }
 }
 
